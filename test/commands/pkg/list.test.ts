b6a85ac8bba2223de956c10d502606170b12afc6
Kellen Hart <kellenhart@gmail.com>
2020-11-15 22:52:08 -0800

    fix root dir definition2

--- test/commands/pkg/list.test.ts
+++ test/commands/pkg/list.test.ts
@@ -1,10 +1,11 @@
 import {expect, test} from '@oclif/test'
-import {cwd} from 'process'
+
+import {testRootDir} from '../../../src/definitions'
 
 describe('manifest:list', () => {
   test
   .stdout()
-  .command(['manifest:list', '--manifestsDir', cwd() + '/test/manifest'])
+  .command(['manifest:list', '--manifestsDir', testRootDir + '/manifest'])
   .it('runs manifest:list finds all', ctx => {
     expect(ctx.stdout).to.contain('test-manifest\ntest-manifest2')
   })
